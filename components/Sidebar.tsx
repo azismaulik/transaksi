77bb0abc9b5b7980c986ec4bdf51be17bb5a8271
azismaulik <azismaulik0@gmail.com>
2024-08-06 23:08:41 +0700

    update: sidebar

--- components/Sidebar.tsx
+++ components/Sidebar.tsx
@@ -11,79 +11,107 @@ import {
 } from "./ui/sheet";
 import { usePathname } from "next/navigation";
 import { cn } from "@/lib/utils";
-import { Menu } from "lucide-react";
+import {
+  ArrowRightLeft,
+  ContactRound,
+  LayoutDashboard,
+  LogOut,
+  Menu,
+  Package,
+} from "lucide-react";
+import { useState } from "react";
+import { Button } from "./ui/button";
 
 const menus = [
   {
     path: "/",
     label: "Dashboard",
+    icon: <LayoutDashboard size={20} />,
   },
   {
     path: "/transaksi",
     label: "Transaksi",
+    icon: <ArrowRightLeft size={20} />,
   },
   {
     path: "/barang",
     label: "Barang",
+    icon: <Package size={20} />,
   },
   {
     path: "/customer",
     label: "Customer",
+    icon: <ContactRound size={20} />,
   },
 ];
 
 const Sidebar = () => {
   const pathname = usePathname();
+  const [isOpen, setIsOpen] = useState(false);
   return (
     <>
-      <Sheet>
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
         <SheetTrigger className="lg:hidden p-4">
           <Menu />
         </SheetTrigger>
-        <SheetContent side="left">
+        <SheetContent
+          side="left"
+          className="flex flex-col justify-between h-screen">
           <SheetHeader>
-            <SheetTitle className="text-4xl font-bold">Azis.</SheetTitle>
+            <SheetTitle className="text-2xl font-bold text-left">
+              Azis.
+            </SheetTitle>
             <SheetDescription>
               <span hidden>Menu navigasi</span>
             </SheetDescription>
           </SheetHeader>
-          <div className="flex flex-col gap-1 w-full mt-10">
+          <div className="mb-auto space-y-3">
             {menus.map((menu) => (
               <Link
                 key={menu.path}
+                onClick={() => setIsOpen(false)}
                 className={cn(
-                  "w-full font-semibold py-2 px-4 rounded hover:bg-primary transition-all hover:text-white",
+                  "w-full font-semibold py-2 hover:px-4 rounded hover:bg-primary transition-all hover:text-white flex gap-4 items-center",
                   {
-                    "bg-primary text-white": pathname === menu.path,
+                    "bg-primary text-white px-4": pathname === menu.path,
                   }
                 )}
-                href={menu.path}
-              >
-                {menu.label}
+                href={menu.path}>
+                <span>{menu.icon}</span>
+                <span>{menu.label}</span>
               </Link>
             ))}
           </div>
+          <Button className="mt-auto">
+            <LogOut className="mr-2 size-5" />
+            Logout
+          </Button>
         </SheetContent>
       </Sheet>
 
-      <aside className="hidden lg:block h-screen sticky top-0 bottom-0 p-4 border-r">
+      <aside className="hidden lg:flex flex-col justify-between h-screen sticky top-0 bottom-0 p-4 border-r shadow">
         <h1 className="text-4xl font-bold">Azis.</h1>
-        <div className="flex flex-col gap-1 w-full mt-10">
+        <div className="flex flex-col gap-2 w-full mt-10">
           {menus.map((menu) => (
             <Link
               key={menu.path}
+              onClick={() => setIsOpen(false)}
               className={cn(
-                "w-full font-semibold py-2 px-4 rounded hover:bg-primary transition-all hover:text-white",
+                "w-full font-semibold py-2 hover:px-4 rounded hover:bg-primary transition-all hover:text-white flex gap-4 items-center",
                 {
-                  "bg-primary text-white": pathname === menu.path,
+                  "bg-primary text-white px-4": pathname === menu.path,
                 }
               )}
-              href={menu.path}
-            >
-              {menu.label}
+              href={menu.path}>
+              <span>{menu.icon}</span>
+              <span>{menu.label}</span>
             </Link>
           ))}
         </div>
+        <Button className="mt-auto">
+          <LogOut className="mr-2 size-5" />
+          Logout
+        </Button>
       </aside>
     </>
   );
